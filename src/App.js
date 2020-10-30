d40a8e08631a2febbfe7d3bdda2cc1f5d71cc6a4
yassun <yassun.engineer.tips@gmail.com>
2020-10-31 02:57:41 +0900

    入門1: Learn React => Learn React from やっすん

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React from やっすん
         </a>
       </header>
     </div>

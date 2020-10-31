5e5ad363b9e6b286f45b43eee4cf9463ac0f1128
yassun <yassun.engineer.tips@gmail.com>
2020-10-31 22:23:57 +0900

    入門3: List.jsのClass Components化

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import { List } from "./List";
 
 class App extends React.Component {
   render() {
     return (
-      <div>クラスコンポーネントにしてみました。</div>
+      <div>
+        クラスコンポーネントにしてみました。
+        <List title="取り扱い言語一覧" />
+      </div>
     )
   }
 }
--- src/List.js
+++ src/List.js
@@ -1,8 +1,14 @@
-export const List = ({ title }) => {
-  return (
-    <div>
-      <h4>{ title }</h4>
-      <div>リストです</div>
-    </div>
-  )
+import React from 'react';
+
+export class List extends React.Component {
+  render() {
+    const { title } = this.props;
+
+    return (
+      <div>
+        <h4>{ title }</h4>
+        <div>リストです</div>
+      </div>
+    )
+  }
 }

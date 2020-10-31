1a1e405730b758a0d6b5f3073ae57107299ce3b1
yassun <yassun.engineer.tips@gmail.com>
2020-10-31 22:15:13 +0900

    入門3: App.jsをClass Componentsに変更

--- src/App.js
+++ src/App.js
@@ -1,20 +1,11 @@
-import { useState } from 'react';
-import { List } from "./List";
+import React from 'react';
 
-function App() {
-  const [description, setDescription] = useState('クリック前の表示');
-
-  const changeDescription = () => {
-    setDescription('クリック後の表示です。')
+class App extends React.Component {
+  render() {
+    return (
+      <div>クラスコンポーネントにしてみました。</div>
+    )
   }
-
-  return (
-    <div>
-      { description }
-      <List title="取り扱い言語一覧"/>
-      <button onClick={changeDescription}>ボタン</button>
-    </div>
-  );
 }
 
 export default App;

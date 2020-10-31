4a94041ae463129246d572dcbee1b3cd909d3b2f
yassun <yassun.engineer.tips@gmail.com>
2020-10-31 22:01:23 +0900

    入門2: stateを使ってみる

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
+import { useState } from 'react';
 import { List } from "./List";
 
 function App() {
+  const [description, setDescription] = useState('クリック前の表示');
+
+  const changeDescription = () => {
+    setDescription('クリック後の表示です。')
+  }
+
   return (
     <div>
-      ゼロから始めるReact入門
+      { description }
       <List title="取り扱い言語一覧"/>
+      <button onClick={changeDescription}>ボタン</button>
     </div>
   );
 }

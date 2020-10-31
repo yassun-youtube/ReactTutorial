00f8aed2c3658104f4e602e76a2356cac159b230
yassun <yassun.engineer.tips@gmail.com>
2020-10-31 14:33:34 +0900

    入門2: コンポーネントの作成と利用

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
+import { List } from "./List";
+
 function App() {
   return (
     <div>
       ゼロから始めるReact入門
+      <List title="取り扱い言語一覧"/>
     </div>
   );
 }
---
+++ src/List.js
@@ -0,0 +1,8 @@
+export const List = ({ title }) => {
+  return (
+    <div>
+      <h4>{ title }</h4>
+      <div>リストです</div>
+    </div>
+  )
+}

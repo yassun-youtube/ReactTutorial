f8b03c6ae9a97e25a288aa8ebee6a8f0580e9d3a
yassun <yassun.engineer.tips@gmail.com>
2020-10-31 23:59:23 +0900

    入門3: ClassComponentsでstateを使ってみる

--- src/App.js
+++ src/App.js
@@ -2,11 +2,24 @@ import React from 'react';
 import { List } from "./List";
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { description: 'クリック前の表示' }
+  }
+
+  changeDescription() {
+    this.setState({
+      description: 'クリック後の表示です。'
+    })
+  }
+
   render() {
+    const { description } = this.state;
     return (
       <div>
-        クラスコンポーネントにしてみました。
+        { description }
         <List title="取り扱い言語一覧" />
+        <button onClick={() => this.changeDescription()}>ボタン</button>
       </div>
     )
   }

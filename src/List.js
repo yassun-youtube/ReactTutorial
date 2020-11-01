1fa9d9b926684e56b2b54f2de154a53176e75553
yassun <yassun.engineer.tips@gmail.com>
2020-11-01 19:42:41 +0900

    入門4: 繰り返しによるリスト表示

--- src/List.js
+++ src/List.js
@@ -1,8 +1,21 @@
+const LANGUAGES = [
+  'JavaScript',
+  'C++',
+  'Ruby',
+  'Java',
+  'PHP',
+  'Go'
+];
+
 export const List = ({ title }) => {
   return (
     <div>
       <h4>{ title }</h4>
-      <div>リストです</div>
+      {
+        LANGUAGES.map((lang, index) => {
+          return <div key={index}>{ lang }</div>
+        })
+      }
     </div>
   )
 }

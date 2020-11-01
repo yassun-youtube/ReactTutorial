6722ba7dba5d721d636faae4e4c022c9af279eac
yassun <yassun.engineer.tips@gmail.com>
2020-11-01 19:49:42 +0900

    入門4: コード整理

--- src/App.js
+++ src/App.js
@@ -3,12 +3,7 @@ import { List } from "./List";
 import { Form } from "./Form";
 
 function App() {
-  const [description, setDescription] = useState('クリック前の表示');
   const [tab, setTab] = useState('list');
-
-  const changeDescription = () => {
-    setDescription('クリック後の表示です。')
-  }
 
   return (
     <div>
@@ -19,11 +14,9 @@ function App() {
         </ul>
       </header>
       <hr />
-      { description }
       {
         tab === 'list' ? <List title="取り扱い言語一覧"/> : <Form />
       }
-      <button onClick={changeDescription}>ボタン</button>
     </div>
   );
 }

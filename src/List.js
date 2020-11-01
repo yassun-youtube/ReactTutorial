20513095b581e2afcc88f2371ba943a691a3f837
yassun <yassun.engineer.tips@gmail.com>
2020-11-02 01:39:54 +0900

    入門6: コード整理

--- src/List.js
+++ src/List.js
@@ -1,13 +1,4 @@
-import { useEffect } from 'react';
-
 export const List = ({ langs }) => {
-  useEffect(() => {
-    console.log('List.js:useEffect');
-    return () => {
-      console.log('List.js:useEffect:unmount')
-    }
-  });
-
   return (
     <div>
       {

1fa65388d4662879c48b15ffd4f0d2ab2c0d915a
yassun <yassun.engineer.tips@gmail.com>
2020-11-01 20:32:31 +0900

    入門5: フォームの利用

--- src/Form.js
+++ src/Form.js
@@ -1,7 +1,24 @@
+import { useState } from 'react';
+
 export const Form = () => {
+  const [text, setText] = useState('');
+
+  const submitForm = (e) => {
+    e.preventDefault();
+    console.log(`submitForm(${text})`);
+  }
+
   return (
     <div>
-      フォームです
+      <h4>新しい言語の追加</h4>
+      <form onSubmit={submitForm}>
+        <div>
+          <input type="text" value={text} onChange={(e) => setText(e.target.value)} />
+        </div>
+        <div>
+          <button>追加</button>
+        </div>
+      </form>
     </div>
   )
 }

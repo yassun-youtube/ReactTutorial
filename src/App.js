b0b098323b1401e464813900c82cd9a8799f3e02
yassun <yassun.engineer.tips@gmail.com>
2020-11-01 19:23:26 +0900

    入門4: タブによる条件分岐

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import { useState } from 'react';
 import { List } from "./List";
+import { Form } from "./Form";
 
 function App() {
   const [description, setDescription] = useState('クリック前の表示');
+  const [tab, setTab] = useState('list');
 
   const changeDescription = () => {
     setDescription('クリック後の表示です。')
@@ -10,8 +12,17 @@ function App() {
 
   return (
     <div>
+      <header>
+        <ul>
+          <li onClick={() => setTab('list')}>リスト</li>
+          <li onClick={() => setTab('form')}>フォーム</li>
+        </ul>
+      </header>
+      <hr />
       { description }
-      <List title="取り扱い言語一覧"/>
+      {
+        tab === 'list' ? <List title="取り扱い言語一覧"/> : <Form />
+      }
       <button onClick={changeDescription}>ボタン</button>
     </div>
   );
---
+++ src/Form.js
@@ -0,0 +1,7 @@
+export const Form = () => {
+  return (
+    <div>
+      フォームです
+    </div>
+  )
+}

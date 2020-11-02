9a6f2508bc2e61019d2962fc6d738d118f73d8d5
yassun <yassun.engineer.tips@gmail.com>
2020-11-02 13:07:39 +0900

    入門7: Form.jsへのスタイルの適用

--- src/Form.js
+++ src/Form.js
@@ -1,4 +1,27 @@
 import { useState } from 'react';
+import styled from 'styled-components';
+import { Button } from "./components/button";
+
+const Container = styled.div`
+  padding: 12px 64px;
+`
+const Label = styled.label`
+  display: flex;
+  color: #757575;
+  font-size: 14px;
+  font-weight: bold;
+`
+const Input = styled.input`
+  border-radius: 3px;
+  padding: 4px 8px;
+  border: 1px solid black;
+`
+const ButtonContainer = styled.div`
+  margin-top: 24px;
+`
+const FormButton = styled(Button)`
+  width: 120px;
+`
 
 export const Form = ({ onAddLang }) => {
   const [text, setText] = useState('');
@@ -9,16 +32,17 @@ export const Form = ({ onAddLang }) => {
   }
 
   return (
-    <div>
+    <Container>
       <h4>新しい言語の追加</h4>
       <form onSubmit={submitForm}>
         <div>
-          <input type="text" value={text} onChange={(e) => setText(e.target.value)} />
+          <Label>言語</Label>
+          <Input type="text" value={text} onChange={(e) => setText(e.target.value)} autoFocus/>
         </div>
-        <div>
-          <button>追加</button>
-        </div>
+        <ButtonContainer>
+          <FormButton>追加</FormButton>
+        </ButtonContainer>
       </form>
-    </div>
+    </Container>
   )
 }

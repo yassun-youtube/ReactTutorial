8632b23752676a6f90322b5d28ce3501cda35e17
yassun <yassun.engineer.tips@gmail.com>
2020-11-02 17:53:46 +0900

    入門9: Portalを利用したモーダルの作成

---
+++ src/components/modal.js
@@ -0,0 +1,36 @@
+import ReactDOM from 'react-dom'
+import { useEffect, useContext } from 'react';
+import styled from 'styled-components';
+
+const modalRoot = document.getElementById('modal-root');
+
+const Container = styled.div`
+  position: absolute;
+  top: 0;
+  left: 0;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100%;
+  width: 100%;
+  background-color: rgba(0, 0, 0, .5);
+`
+
+export const Modal = (props) => {
+  const elem = document.createElement('div');
+
+  useEffect(() => {
+    modalRoot.appendChild(elem);
+
+    return () => {
+      modalRoot.removeChild(elem);
+    }
+  })
+
+  return ReactDOM.createPortal(
+    <Container>
+      { props.children }
+    </Container>,
+    elem
+  )
+}

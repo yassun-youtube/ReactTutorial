4bc2eb6c3d63777eeef7cf563a60942d17bad3c0
yassun <yassun.engineer.tips@gmail.com>
2020-11-02 13:06:58 +0900

    入門7: List.jsへのスタイルの適用

--- src/List.js
+++ src/List.js
@@ -1,11 +1,28 @@
+import styled from 'styled-components';
+
+const Component = styled.div`
+  padding: 12px 64px;
+`
+
+const ListItem = styled.div`
+  padding: 8px 16px;
+  
+  &:nth-child(n+2) {
+    border-top: 1px solid #D9DBDE;
+  }
+`
+
 export const List = ({ langs }) => {
   return (
-    <div>
-      {
-        langs.map((lang, index) => {
-          return <div key={index}>{ lang }</div>
-        })
-      }
-    </div>
+    <Component>
+      <h4>取り扱い言語リスト</h4>
+      <div>
+        {
+          langs.map((lang, index) => {
+            return <ListItem key={index}>{ lang }</ListItem>
+          })
+        }
+      </div>
+    </Component>
   )
 };

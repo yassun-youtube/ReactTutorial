f3bd987ff32d289a8b55b1a247fe394cc607fa68
yassun <yassun.engineer.tips@gmail.com>
2020-11-02 12:52:23 +0900

    入門7: App.jsへのスタイルの適用

--- src/App.js
+++ src/App.js
@@ -1,7 +1,27 @@
 import { useState, useEffect } from 'react';
 import { List } from "./List";
 import { Form } from "./Form";
+import styled from 'styled-components';
 import { getLanguages } from "./const/languages";
+
+const Header = styled.header`
+  display: flex;
+  justify-content: space-between;
+  padding: 24px 64px 0;
+  border-bottom: 1px solid #E0E0E0;
+`
+
+const HeaderUl = styled.ul`
+  display: flex;
+  margin: 0;
+  padding: 0;
+`
+const HeaderLi = styled.li`
+  list-style: none;
+  padding: 4px 12px;
+  cursor: pointer;
+  border-bottom: ${props => props.focused ? '2px solid #F44336' : 'none' };
+`
 
 function App() {
   const [tab, setTab] = useState('list');
@@ -24,13 +44,12 @@ function App() {
 
   return (
     <div>
-      <header>
-        <ul>
-          <li onClick={() => setTab('list')}>リスト</li>
-          <li onClick={() => setTab('form')}>フォーム</li>
-        </ul>
-      </header>
-      <hr />
+      <Header>
+        <HeaderUl>
+          <HeaderLi focused={tab === 'list'} onClick={() => setTab('list')}>リスト</HeaderLi>
+          <HeaderLi focused={tab === 'form'} onClick={() => setTab('form')}>フォーム</HeaderLi>
+        </HeaderUl>
+      </Header>
       {
         tab === 'list' ? <List langs={langs} /> : <Form onAddLang={addLang}/>
       }

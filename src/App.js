44355e3e83ef93e553c73eeb71fffea626b18c3e
yassun <yassun.engineer.tips@gmail.com>
2020-11-03 16:44:01 +0900

    入門11: テーマ適用前の準備

--- src/App.js
+++ src/App.js
@@ -1,28 +1,10 @@
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
 import { List } from "./List";
 import { Form } from "./Form";
 import styled from 'styled-components';
 import { getLanguages } from "./const/languages";
 import { withLoading } from "./hoc/with-loading";
-
-const Header = styled.header`
-  display: flex;
-  justify-content: space-between;
-  padding: 24px 64px 0;
-  border-bottom: 1px solid #E0E0E0;
-`
-
-const HeaderUl = styled.ul`
-  display: flex;
-  margin: 0;
-  padding: 0;
-`
-const HeaderLi = styled.li`
-  list-style: none;
-  padding: 4px 12px;
-  cursor: pointer;
-  border-bottom: ${props => props.focused ? '2px solid #F44336' : 'none' };
-`
+import { Header } from "./Header";
 
 function App({ data }) {
   const [tab, setTab] = useState('list');
@@ -35,15 +17,10 @@ function App({ data }) {
 
   return (
     <div>
-      <Header>
-        <HeaderUl>
-          <HeaderLi focused={tab === 'list'} onClick={() => setTab('list')}>リスト</HeaderLi>
-          <HeaderLi focused={tab === 'form'} onClick={() => setTab('form')}>フォーム</HeaderLi>
-        </HeaderUl>
-      </Header>
-      {
-        tab === 'list' ? <List langs={langs} /> : <Form onAddLang={addLang}/>
-      }
+        <Header tab={tab} setTab={setTab} />
+        {
+          tab === 'list' ? <List langs={langs} /> : <Form onAddLang={addLang}/>
+        }
     </div>
   );
 }

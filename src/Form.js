2d219920090704167a7aa83400eac36559551a87
yassun <yassun.engineer.tips@gmail.com>
2020-11-18 21:15:54 +0900

    入門10: refを使ったfocus

--- src/Form.js
+++ src/Form.js
@@ -3,6 +3,7 @@ import styled from 'styled-components';
 import { Button } from "./components/button";
 import { TabBodyContainer } from "./components/tab-body-container";
 import { FormModal } from "./FormModal";
+import { Hint } from "./Hint";
 
 const Label = styled.label`
   display: flex;
@@ -41,6 +42,7 @@ export class Form extends React.Component {
           <div>
             <Label>言語</Label>
             <Input type="text" value={text} onChange={(e) => this.setState({ text: e.target.value })} autoFocus/>
+            <Hint />
           </div>
           <ButtonContainer>
             <FormButton>追加</FormButton>

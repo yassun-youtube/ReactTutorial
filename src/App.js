0bcc7af69139a265502ef4e6f44670b0a8f5877a
yassun <yassun.engineer.tips@gmail.com>
2020-11-19 21:25:36 +0900

    入門11: テーマ適用前の準備

--- src/App.js
+++ src/App.js
@@ -4,25 +4,7 @@ import { Form } from "./Form";
 import styled from 'styled-components';
 import { getLanguages } from "./const/languages";
 import { withLoading } from "./hoc/with-loading";
-
-const Header = styled.header`
-  display: flex;
-  justify-content: space-between;
-  padding: 24px 64px 0;
-  border-bottom: 1px solid #E0E0E0;
-`
-
-const HeaderUl = styled.ul`
-  display: flex;
-  margin: 0;
-  padding: 0;
-`
-const HeaderLi = styled.li`
-  list-style: none;
-  padding: 4px 12px;
-  cursor: pointer;
-  border-bottom: ${props => props.focused ? '2px solid #F44336' : 'none' };
-`
+import { Header } from "./Header";
 
 function App({ data }) {
   const [tab, setTab] = useState('list');
@@ -35,12 +17,7 @@ function App({ data }) {
 
   return (
     <div>
-      <Header>
-        <HeaderUl>
-          <HeaderLi focused={tab === 'list'} onClick={() => setTab('list')}>リスト</HeaderLi>
-          <HeaderLi focused={tab === 'form'} onClick={() => setTab('form')}>フォーム</HeaderLi>
-        </HeaderUl>
-      </Header>
+      <Header tab={tab} setTab={setTab}/>
       {
         tab === 'list' ? <List langs={langs} /> : <Form onAddLang={addLang}/>
       }
---
+++ src/Header.js
@@ -0,0 +1,31 @@
+import styled from "styled-components";
+
+const Container = styled.header`
+  display: flex;
+  justify-content: space-between;
+  padding: 24px 64px 0;
+  border-bottom: 1px solid #E0E0E0;
+`
+
+const HeaderUl = styled.ul`
+  display: flex;
+  margin: 0;
+  padding: 0;
+`
+const HeaderLi = styled.li`
+  list-style: none;
+  padding: 4px 12px;
+  cursor: pointer;
+  border-bottom: ${props => props.focused ? '2px solid #F44336' : 'none' };
+`
+
+export const Header = ({ tab, setTab }) => {
+  return (
+    <Container>
+      <HeaderUl>
+        <HeaderLi focused={tab === 'list'} onClick={() => setTab('list')}>リスト</HeaderLi>
+        <HeaderLi focused={tab === 'form'} onClick={() => setTab('form')}>フォーム</HeaderLi>
+      </HeaderUl>
+    </Container>
+  )
+}
